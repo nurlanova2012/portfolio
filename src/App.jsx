188b303c1e5ca4bace8427e844fddcce6f621c0b
Aizirek Nurlanbekova <aizirek.nurlanbekova.pls@gmail.com>
2021-10-05 15:21:23 +0600

    lesson-61

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-import { Testimonials, Intro, Works, Portfolio, Contacts, TopBar } from './components';
+import { Testimonials, Intro, Works, Portfolio, Contacts, TopBar, Menu } from './components';
 import './app.scss';
 
 function App() {
@@ -9,13 +9,24 @@ function App() {
   return (
     <div className="app">
     <TopBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-
+    <Menu items={[
+      {name: "home", link: "intro"},
+      {name: "portfolio", link: "portfolio"},
+      {name: "works", link: "works"},
+      {name: "testimonials", link: "testimonials"},
+      {name: "contacts", link: "contacts"},
+    ]}
+    
+    menuOpen={menuOpen}
+    setMenuOpen={setMenuOpen}
+    />
     <div className="app__sections">
     <Intro/>
     <Portfolio/>
     <Works/>
     <Testimonials/>
     <Contacts/>
+    
 
 
     </div>
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,46 @@
+import {useEffect, useRef} from "react";
+import {init} from "ityped";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import "./intro.scss";
 
 function Intro() {
+   const animatedTextRef = useRef();
+
+   useEffect(()=> {
+       init(animatedTextRef.current, {
+           showCursor: true,
+           backDelay: 1500,
+           backSpeed: 60,
+           strings: ["React. JS Developer"]
+       })
+   }, []);
+
     return (
        <section className="intro" id="intro">
-           Intro
+        <div className="intro__left">
+            <div className="intro__img-wrapper">
+            <img src="https://media.istockphoto.com/photos/profile-side-photo-of-concentrated-skilled-web-expert-afro-american-picture-id1218017051?b=1&k=20&m=1218017051&s=170667a&w=0&h=5W2TR300wgYONVieb-HBSLYi9jPkOVJcoYoTJWcB2Ms=" alt="pic of coder" />
+
+            </div>
+        </div>
+        <div className="intro__right">
+            <div className="intro__right-inner">
+            <h1>Aizirek Nurlanbekova</h1>
+            <h2>Coding</h2>
+            <h3>Design</h3>
+            <h4>
+                <span ref={animatedTextRef} >
+                    
+                </span>
+            </h4>
+            <a href="#portfolio">
+                <KeyboardArrowDownIcon className="intro__icon"/>
+            </a>
+
+            </div>
+        </div>
        </section>
     )
 }
 
-export default Intro
+export default Intro;
